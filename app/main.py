466d6856028eab3c2ec6be9fb394886d8cc2e9a5
sridhar <140493914+sridhar-vs@users.noreply.github.com>
2024-12-30 22:10:29 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/main")
-async def root():
-    return {"message": "Hello, Catalyst!"}
+@app.get("/api")
+def read_api():
+    return {"message": "Hello from main.py"}
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from the root of main.py"}
